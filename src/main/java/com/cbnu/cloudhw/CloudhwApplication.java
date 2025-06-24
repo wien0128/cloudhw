ff8f1338c8f90fbd785151a8b8d1611e644eee4d
Hyunbin Kim <taque88@naver.com>
2025-06-24 14:56:12 +0900

    Initial commit

---
+++ src/main/java/com/cbnu/cloudhw/CloudhwApplication.java
@@ -0,0 +1,13 @@
+package com.cbnu.cloudhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudhwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudhwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cbnu/cloudhw/CloudhwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbnu.cloudhw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudhwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
